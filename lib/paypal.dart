536d7d22c967e9a4faf6a778871d91df435de798
Ibrahim Elbaz <ibrahim.elbaz.1011@gmail.com>
2024-09-27 13:58:55 +0300

    first commit

---
+++ lib/paypal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Paypal extends StatelessWidget {
+  const Paypal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
